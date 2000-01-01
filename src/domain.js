a24fa2f5892a6542acabcf53b478e111e707138c
Pairwise baseline migration <pairwise-baseline@localhost>
2000-01-01 00:00:00 +0000

    A/B baseline migrated from 6de6c66c71b346cfe53359482cab77f62bf0adce

---
+++ src/domain.js
@@ -0,0 +1,4 @@
+export const contributionKinds = ['authoring', 'review', 'classroom-validation', 'coordination'];
+export const versionStates = ['working', 'frozen', 'authorized', 'published'];
+export const disputeStates = ['raised', 'responded', 'resolved', 'withdrawn'];
+export const confirmationStates = ['pending', 'agreed', 'objected'];
